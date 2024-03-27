80d6b4f650ff6035e69626d954d7050398007694
Frontend JavaScript <frontendjavascript@iMac-02.local>
2024-03-27 20:43:24 +0200

    curs5

---
+++ curs5/app1/user.mjs
@@ -0,0 +1,10 @@
+let users;
+
+(async () => {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const response = await fetch(url);
+  users = await response.json();
+  
+})();
+
+export { users };
--- cvtemplate/webpack.config.js
+++ cvtemplate/webpack.config.js
@@ -18,6 +18,7 @@ const myBanner = fs.readFileSync(paths.source + '/views/banner.html');
 const myAbout = fs.readFileSync(paths.source + '/views/about.html');
 const myExp = fs.readFileSync(paths.source + '/views/experience.html');
 const myPort = fs.readFileSync(paths.source + '/views/portofolio.html');
+const myGitHub=fs.readFileSync(paths.source + '/views/githubapi.html');
 const myContact = fs.readFileSync(paths.source + '/views/contact.html');
 
 const myFooter = fs.readFileSync(paths.source + '/views/footer.html');
@@ -43,6 +44,7 @@ module.exports = {
             myExp: myExp,
             myPortofolio: myPort,
             myContact: myContact,
+            gitHubApi: myGitHub,
             myFooter: myFooter,
             template: './src/index.html',
             filename: 'index.html',
